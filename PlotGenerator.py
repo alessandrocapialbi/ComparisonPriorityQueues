fe05ef7e34cdb4c9ac52011863d17abd6248a532
Alessandro Capialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2024-04-18 13:11:37 +0200

    Final commit

--- PlotGenerator.py
+++ PlotGenerator.py
@@ -11,7 +11,7 @@ class PlotGenerator:
     def __init__(self):
         pass
 
-    def measure_insertion_time(self, pq, n, timeCalculation, randomRange=1000):
+    def measure_insertion_time(self, pq, n, timeCalculation, randomRange=10000):
         if (isinstance(pq, HeapPriorityQueue)):
             name = "Heap"
         elif (isinstance(pq, UnsortedLinkedListPriorityQueue)):
@@ -30,6 +30,30 @@ class PlotGenerator:
                 times[i + 1] = insertion_time + times[i]
             else:
                 times[i + 1] = (insertion_time / pq.get_size()) + times[i]
+
+        end_time = time.time()
+        print(name + f" insertion time: {end_time - s}")
+        return times
+
+    def measure_insertion_time_2(self, pq, n, timeCalculation, randomRange=10000):
+        if (isinstance(pq, HeapPriorityQueue)):
+            name = "Heap"
+        elif (isinstance(pq, UnsortedLinkedListPriorityQueue)):
+            name = "Unsorted List"
+        elif (isinstance(pq, SortedLinkedListPriorityQueue)):
+            name = "Sorted List"
+        else:
+            name = "Unknown"
+        times = [0] * (n + 1)  # Initialize the list with zeros
+        s = time.time()
+        for i in range(0, n):
+            start_time = time.perf_counter()
+            pq.insert(random.randint(0, randomRange))  # Insert random values
+            insertion_time = (time.perf_counter() - start_time)
+            if (not timeCalculation):
+                times[i + 1] = insertion_time + times[i]
+            else:
+                times[i + 1] = (insertion_time / pq.get_size())
 
         end_time = time.time()
         print(name + f" insertion time: {end_time - s}")
@@ -90,7 +114,7 @@ class PlotGenerator:
 
         return times
 
-    def measure_extraction_time_3(self, pq, n, window_size=100):
+    def measure_extraction_time_3(self, pq, n):
         if (isinstance(pq, HeapPriorityQueue)):
             name = "Priority Queue"
         elif (isinstance(pq, UnsortedLinkedListPriorityQueue)):
@@ -106,11 +130,10 @@ class PlotGenerator:
             start_time = time.perf_counter()
             pq.extract_max()  # Extract max value
             extraction_time = (time.perf_counter() - start_time)
-            times[i + 1] = extraction_time
-
-        # Calculate the moving average
-        if i >= window_size:
-            moving_averages[i + 1] = sum(times[(i + 1) - window_size:(i + 1)]) / window_size
+            if (pq.get_size() != 0):
+                times[i + 1] = (extraction_time / pq.get_size())
+            else:
+                times[i + 1] = extraction_time
 
         end_time = time.time()
         print(f"\nn = {n} " + name + f" extraction time: {end_time - s}")
@@ -128,6 +151,7 @@ class PlotGenerator:
         plt.xlim(left=0)
         plt.ylim(bottom=0)
         plt.legend()
+        plt.savefig(f'{label}.png')  # Save the figure
         plt.show()
 
     def compare_plots(self, data, n, title, colors):
@@ -140,4 +164,5 @@ class PlotGenerator:
         plt.xlim(left=0)
         plt.ylim(bottom=0)
         plt.legend()
+        plt.savefig(f'{title}.png')  # Save the figure
         plt.show()
--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     extraction_pq_heap_times = plt.measure_extraction_time_2(pq_heap, n, True)
     plt.plot(extraction_pq_heap_times, n, 'Heap Extraction', 'red')
 
-    insertion_pq_unsorted_list_times = plt.measure_insertion_time(pq_unsorted_list, n, True)
+    insertion_pq_unsorted_list_times = plt.measure_insertion_time_2(pq_unsorted_list, n, True)
     plt.plot(insertion_pq_unsorted_list_times, n, 'Unsorted List Insertion', 'blue')
 
     extraction_pq_unsorted_list_times = plt.measure_extraction_time(pq_unsorted_list, n, True)
@@ -36,3 +36,9 @@ if __name__ == "__main__":
         (insertion_pq_unsorted_list_times, 'Unsorted List Insertion'),
         (insertion_pq_sorted_list_times, 'Sorted List Insertion')
     ], n, 'Insertion Time Comparison', ['red', 'blue', 'green'])
+
+    plt.compare_plots([
+        (extraction_pq_heap_times, 'Heap Extraction'),
+        (extraction_pq_unsorted_list_times, 'Unsorted List Extraction'),
+        (extraction_pq_sorted_list_times, 'Sorted List Extraction')
+    ], n, 'Extraction Time Comparison', ['red', 'blue', 'green'])
